dcb05210015574222cf1242db72012f8336cb534
Shervin.V.Q <shervinvahidian1994@gmail.com>
2024-03-12 13:34:52 +0100

    second version of multiplication ( for several number)

--- multiplicationtable.py
+++ multiplicationtable.py
@@ -1,3 +1,9 @@
 a= int(input("write first number:"))
 b= int(input("write second number:"))
-print(f"result of multiplication is {a*b}")
+c=a*b
+d=input("do you want to add number? yes/no?")
+while d=="yes":
+    z=int(input("write number:"))
+    c=c*z
+    d=input("do you want to add number? yes/no?")
+print(f"result of multiplication is {c}")
